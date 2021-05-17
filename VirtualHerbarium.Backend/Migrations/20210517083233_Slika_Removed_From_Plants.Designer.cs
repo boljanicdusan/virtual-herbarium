003d6f4405d2ae9e5d2a6c7416fa7cd9bb10da68
Dusan Boljanic <dusan@digitalcontrol.me>
2021-05-17 10:36:43 +0200

    Removed Columns: Slika and SlikaUPrirodi

--- VirtualHerbarium.Backend/Migrations/20210517083233_Slika_Removed_From_Plants.Designer.cs
+++ VirtualHerbarium.Backend/Migrations/20210517083233_Slika_Removed_From_Plants.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VirtualHerbarium.Backend.Entities;
 
 namespace VirtualHerbarium.Backend.Migrations
 {
     [DbContext(typeof(VirtualHerbariumDbContext))]
-    partial class VirtualHerbariumDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210517083233_Slika_Removed_From_Plants")]
+    partial class Slika_Removed_From_Plants
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VirtualHerbarium.Backend/Migrations/20210517083233_Slika_Removed_From_Plants.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VirtualHerbarium.Backend.Migrations
+{
+    public partial class Slika_Removed_From_Plants : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Slika",
+                table: "Biljke");
+
+            migrationBuilder.DropColumn(
+                name: "SlikaUPrirodi",
+                table: "Biljke");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slika",
+                table: "Biljke",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SlikaUPrirodi",
+                table: "Biljke",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
