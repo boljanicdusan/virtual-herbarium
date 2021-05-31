a788b27602e731b187a39ed09559786a57e87420
Dusan Boljanic <dusan@digitalcontrol.me>
2021-05-31 16:36:40 +0200

    Folder renamed to images

--- VirtualHerbarium.Backend/Controllers/AuthController.cs
+++ VirtualHerbarium.Backend/Controllers/AuthController.cs
@@ -55,7 +55,7 @@ namespace VirtualHerbarium.Backend.Controllers
             var token = new JwtSecurityToken(_config["Jwt:Issuer"],
                 _config["Jwt:Issuer"],
                 null,
-                expires: DateTime.Now.AddMinutes(120),
+                expires: DateTime.Now.AddHours(24),
                 signingCredentials: credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
